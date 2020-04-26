8b2386361cceac2128a68005e09b6f3df767839b
jeyroik <jeyroik@gmail.com>
2020-04-26 20:39:35 +0300

    rm string support

--- src/interfaces/IHasUpdatedAt.php
+++ src/interfaces/IHasUpdatedAt.php
@@ -19,7 +19,7 @@ interface IHasUpdatedAt
     public function getUpdatedAt(string $format = '');
 
     /**
-     * @param string|int|\DateTime $time
+     * @param int|\DateTime $time
      * @return $this
      */
     public function setUpdatedAt($time);
