00e0ab56650294b965eb34e07d57063a591e24b3
jeyroik <jeyroik@gmail.com>
2020-05-15 12:18:33 +0300

    added IDispatcherWrapper

---
+++ src/components/DispatcherWrapper.php
@@ -0,0 +1,17 @@
+<?php
+namespace extas\components;
+
+use extas\interfaces\IDispatcherWrapper;
+
+/**
+ * Class DispatcherWrapper
+ *
+ * @package extas\components
+ * @author jeyroik@gmail.com
+ */
+class DispatcherWrapper implements IDispatcherWrapper
+{
+    use THasName;
+    use THasDescription;
+    use THasClass;
+}
